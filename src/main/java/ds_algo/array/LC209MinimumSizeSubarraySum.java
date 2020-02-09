7294efd7ddfa09615c1f581716e80311a74c78f7
Raqeebul Ketan <bijoy.ketan@gmail.com>
2020-02-09 12:22:06 -0500

    Completed LC:209 Minimum Size Subarray Sum

---
+++ src/main/java/ds_algo/array/LC209MinimumSizeSubarraySum.java
@@ -0,0 +1,49 @@
+package ds_algo.array;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class LC209MinimumSizeSubarraySum {
+
+    public int minSubArrayLen(int s, int[] nums) {
+        List<Integer> countList = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] >= s) return 1;
+            int sum = nums[i];
+            int count = 1;
+            for (int j = i + 1; j < nums.length; j++) {
+                sum += nums[j];
+                count++;
+                if (sum >= s) {
+                    countList.add(count);
+                    break;
+                }
+            }
+        }
+        Collections.sort(countList);
+        if (countList.size() > 0) return countList.get(0);
+        return 0;
+
+    }
+
+    @Test
+    public void testBase () {
+        assertEquals(2, minSubArrayLen(7, new int[]{2,3,1,2,4,3}));
+    }
+
+    @Test
+    public void test2 () {
+        assertEquals(3, minSubArrayLen(11, new int[]{1,2,3,4,5}));
+    }
+
+    @Test
+    public void testEmptyArr () {
+        assertEquals(0, minSubArrayLen(5, new int[]{}));
+    }
+
+}
