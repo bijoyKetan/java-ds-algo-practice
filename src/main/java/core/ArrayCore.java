dbfe5ffd3fbd66c0fb5cffe9faf3babdc251ab02
Raqeebul Ketan <bijoy.ketan@gmail.com>
2020-03-01 11:35:29 -0500

    Enhanced the core array class to illustrate commonly used methods.

--- src/main/java/core/ArrayCore.java
+++ src/main/java/core/ArrayCore.java
@@ -1,47 +1,43 @@
 package core;
 
-import java.sql.SQLOutput;
-import java.util.Arrays;
-
-/*
-String [] myArray = {"val1", "val2", "val3"};
-myArray.length;
-firstItem  = myArray[1];
-lastItem = myArray[myArray.length -1];
-for (String i:myArray) {expression};
-for (int i = 0; i < myArray.length; i++) {expression};
-*/
+import java.util.*;
 
 public class ArrayCore {
 
     public static void main(String[] args) {
 
-        System.out.print("");
-        System.out.println("");
         //Create the DS
-        String[] myArray = {"Asia", "Europe", "Africa", "America", "Australia"};
-        System.out.println("Array1 is -> " + Arrays.toString(myArray));
+        String[] strArr = {"Asia", "Europe", "Africa", "America", "Australia"};
 
-        String[] myArray4 = new String[]{"val1", "val2", "val3"};
-        String[] myArray6 = new String[]{"val1", "val2", "val3"};
+        //Iterate through and get first and last items
+        Map<String, Object> arrMap = new HashMap<>();
+        arrMap.put("FirstItem", strArr[0]);
+        arrMap.put("LastItem", strArr[strArr.length - 1]);
+        arrMap.put("StringArray", new ArrayList());
 
-        String[] yourArray = new String[5];
-        yourArray[0] = "Africa";
-        yourArray[1] = "Asia";
-        yourArray[2] = "North America";
-        yourArray[3] = "Australia";
-        yourArray[4] = "Europe";
-        System.out.println("Array 2 is -> " + Arrays.toString(yourArray));
+        for (int i = 0; i < strArr.length; i++) {
+            List<String> s = (List<String>) arrMap.get("StringArray");
+            s.add(strArr[i]);
+            arrMap.put("StringArray", s);
+        }
 
-        //Access first and last item
-        String firstItem = myArray[0];
-        String lastItem = myArray[myArray.length - 1];
-        System.out.println("The first item is: -> " + firstItem + "\nThe last item is -> " + lastItem);
+        //Copy array
+        String[] copyArr = Arrays.copyOfRange(strArr, 0, strArr.length);
 
-        //Array Copy
-        int[] arr1 = {1, 2, 3, 4, 5};
-        int[] arr2 = {6, 7, 8, 9, 10};
-        System.arraycopy(arr1, 2, arr2, 2, 3); //-> [6,7,3,4,5]
-        System.out.println(Arrays.toString(arr2));
+        //Reverse string with char[]
+        String str = "FooBar";
+        char[] charArr = str.toCharArray();
+        for (int i = 0; i < charArr.length / 2; i++) {
+            char temp = charArr[i];
+            charArr[i] = charArr[charArr.length - i - 1];
+            charArr[charArr.length - i - 1] = temp;
+        }
+        String reversedString = new String(charArr);
+
+
+        //Print results
+        System.out.println("arrMap: " + arrMap + "\n");
+        System.out.println("Array copy: " + Arrays.toString(copyArr) + "\n");
+        System.out.println("Reserved String: " + reversedString + "\n");
     }
 }
--- src/main/java/ds_algo/map/LC359LoggerRateLimiter.java
+++ src/main/java/ds_algo/map/LC359LoggerRateLimiter.java
@@ -7,7 +7,7 @@ public class LC359LoggerRateLimiter {
     //Design a logger system that receive stream of messages along with its timestamps,
     // each message should be printed if and only if it is not printed in the last 10 seconds.
 
-
+    //TODO - Implement this
     public LC359LoggerRateLimiter() {
         Map<String, Integer> loggerMap = new HashMap<>();
     }
