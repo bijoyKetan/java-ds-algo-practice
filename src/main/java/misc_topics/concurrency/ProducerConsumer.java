3096fca17080349f68e06ba821f82171c298e7fe
Raqeebul Ketan <bijoy.ketan@gmail.com>
2020-03-16 00:00:32 -0400

    Refactored the ProducerConsumer code.

--- src/main/java/misc_topics/concurrency/ProducerConsumer.java
+++ src/main/java/misc_topics/concurrency/ProducerConsumer.java
@@ -1,6 +1,8 @@
 package misc_topics.concurrency;
 
 import java.util.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.ReentrantLock;
 
@@ -8,12 +10,15 @@ public class ProducerConsumer {
     public static void main(String[] args) {
         Buffer sharedBuffer = new Buffer(Arrays.asList(1, 2));
         ReentrantLock bufferLock = new ReentrantLock();
+        Condition condition = bufferLock.newCondition();
 
-        MyProducer p1 = new MyProducer(ThreadColor.ANSI_CYAN, sharedBuffer, 2, bufferLock);
-        MyConsumer c1 = new MyConsumer(ThreadColor.ANSI_RED, sharedBuffer, bufferLock);
+        ExecutorService executorService = Executors.newFixedThreadPool(5);
 
-        new Thread(p1).start();
-        new Thread(c1).start();
+        MyProducer p1 = new MyProducer(ThreadColor.ANSI_CYAN, sharedBuffer, 2, bufferLock, condition);
+        MyConsumer c1 = new MyConsumer(ThreadColor.ANSI_RED, sharedBuffer, bufferLock, condition);
+
+        executorService.execute(p1);
+        executorService.execute(c1);
     }
 }
 
@@ -24,27 +29,39 @@ class MyProducer implements Runnable {
 
     private String color;
     private final Buffer buffer;
-    int startingPoint ;
+    int startingPoint;
     private final ReentrantLock bufferLock;
+    private Condition condition;
 
-    public MyProducer(String color, Buffer buffer, int startingPoint, ReentrantLock bufferLock) {
+    public MyProducer(String color, Buffer buffer, int startingPoint, ReentrantLock bufferLock, Condition condition) {
         this.color = color;
         this.buffer = buffer;
         this.startingPoint = startingPoint;
         this.bufferLock = bufferLock;
+        this.condition = condition;
     }
 
     @Override
     public void run() {
 
         while (true) {
-         bufferLock.lock();
+            bufferLock.lock();
             if (!buffer.isFull()) {
                 try {
                     buffer.addItemToBuffer(startingPoint++);
                     System.out.println(color + "Producer " + Thread.currentThread().getName() + " added: " + startingPoint + "\n------------------------------\n");
+//                    condition.await();
                     bufferLock.unlock();
                     Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                System.out.println("Buffer full, producer can't add to buffer...");
+//            condition.signalAll();
+                try {
+                    Thread.sleep(500);
+                    bufferLock.unlock();
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
@@ -61,11 +78,13 @@ class MyConsumer implements Runnable {
     private String color;
     private Buffer buffer;
     private ReentrantLock bufferLock;
+    private Condition condition;
 
-    public MyConsumer(String color, Buffer buffer, ReentrantLock bufferLock) {
+    public MyConsumer(String color, Buffer buffer, ReentrantLock bufferLock, Condition condition) {
         this.color = color;
         this.buffer = buffer;
         this.bufferLock = bufferLock;
+        this.condition = condition;
 
     }
 
@@ -73,17 +92,28 @@ class MyConsumer implements Runnable {
     public void run() {
 
         while (true) {
-        bufferLock.lock();
+            bufferLock.lock();
             if (!buffer.isEmpty()) {
                 try {
                     System.out.println(color + "Consumer " + Thread.currentThread().getName() + " consumed: " + buffer.removeItemFromBuffer() + "\n------------------------------\n");
+//                    condition.await();
                     bufferLock.unlock();
                     Thread.sleep(500);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-            }
+            } else {
+                System.out.println("Buffer empty, consumer cannot consume...");
 
+//                condition.signalAll();
+                try {
+                    Thread.sleep(500);
+                    bufferLock.unlock();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+            }
         }
     }
 
@@ -102,9 +132,9 @@ class Buffer {
         if (items.size() > max_size) {
             throw new IllegalArgumentException("Max queue size is 10");
         }
-            this.buffer = new LinkedList<>();
-            for (Integer i : items) {
-                buffer.offer(i);
+        this.buffer = new LinkedList<>();
+        for (Integer i : items) {
+            buffer.offer(i);
         }
     }
 
@@ -116,16 +146,16 @@ class Buffer {
         return (buffer.size() == max_size);
     }
 
-    public void addItemToBuffer(int a){
+    public void addItemToBuffer(int a) {
         buffer.add(a);
     }
 
-    public int removeItemFromBuffer(){
+    public int removeItemFromBuffer() {
         return buffer.poll();
     }
 
-    public List<Integer> getBufferValues(){
-    List<Integer> bufferValuesToList = new ArrayList<>();
+    public List<Integer> getBufferValues() {
+        List<Integer> bufferValuesToList = new ArrayList<>();
         bufferValuesToList.addAll(this.buffer);
         return bufferValuesToList;
     }
