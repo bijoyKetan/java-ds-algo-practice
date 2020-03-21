cc0c9210e3f0e0bf8dae03152c9c95e46212a251
Raqeebul Ketan <bijoy.ketan@gmail.com>
2020-03-20 21:33:34 -0400

    Added code to demonstrate how deadlock can be avoided.

--- src/main/java/misc_topics/concurrency/Deadlock.java
+++ src/main/java/misc_topics/concurrency/Deadlock.java
@@ -20,11 +20,14 @@ public class Deadlock {
                 }
                 System.out.println("Thread " + Thread.currentThread().getName() + " waiting for lock 2...");
                 synchronized (lock2) {
-                    System.out.println("Thread " + Thread.currentThread().getName() + " acquired lock 2 and running...");
+                    System.out.println("Thread " + Thread.currentThread().getName() + " acquired lock 1 and lock 2 and running...");
                 }
+                System.out.println("Thread " + Thread.currentThread().getName() + " release lock 2");
             }
+            System.out.println("Thread " + Thread.currentThread().getName() + " release lock 1 and exiting...");
         };
 
+        //Code for deadlock
         Runnable t2 = () -> {
             synchronized (lock2) {
                 System.out.println("Thread " + Thread.currentThread().getName() + " acquired lock 2 and running...");
@@ -35,10 +38,30 @@ public class Deadlock {
                 }
                 System.out.println("Thread " + Thread.currentThread().getName() + " waiting for lock 1...");
                 synchronized (lock1) {
-                    System.out.println("Thread " + Thread.currentThread().getName() + " acquired lock 1 and running...");
+                    System.out.println("Thread " + Thread.currentThread().getName() + " acquired lock 1 and lock 2 and running...");
                 }
+                System.out.println("Thread " + Thread.currentThread().getName() + " release lock 1");
             }
+            System.out.println("Thread " + Thread.currentThread().getName() + " release lock 2 and exiting...");
         };
+
+//        //Code for avoiding deadlock
+//        Runnable t2 = () -> {
+//            synchronized (lock1) {
+//                System.out.println("Thread " + Thread.currentThread().getName() + " acquired lock 1 and running...");
+//                try {
+//                    Thread.sleep(1000);
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                }
+//                System.out.println("Thread " + Thread.currentThread().getName() + " waiting for lock 2...");
+//                synchronized (lock2) {
+//                    System.out.println("Thread " + Thread.currentThread().getName() + " acquired lock 1 and lock 2 and running...");
+//                }
+//                System.out.println("Thread " + Thread.currentThread().getName() + " release lock 2");
+//            }
+//            System.out.println("Thread " + Thread.currentThread().getName() + " release lock 1 and exiting...");
+//        };
 
         new Thread(t1).start();
         new Thread(t2).start();
