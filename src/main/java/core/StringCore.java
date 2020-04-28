1f7a3ed45d34b9800a9ee7539bfaf8afecd5c2ff
Raqeebul Ketan <bijoy.ketan@gmail.com>
2020-04-28 00:38:39 -0400

    Implemented palindrome check using iterative method.

--- src/main/java/core/StringCore.java
+++ src/main/java/core/StringCore.java
@@ -1,22 +1,22 @@
 package core;
 
-    /**
-    Common String methods:
-    charAt(i) -> the character in this String at a given index
-    contains(str) -> true if this String contains the other's characters inside it
-    endsWith(str) -> true if this String ends with the other's characters
-    equals(str) -> true if this String is the same as str
-    equalsIgnoreCase(str) -> true if this String is the same as str, ignoring capitalization
-    indexOf(str) -> first index in this String where given String begins (-1 if not found)
-    lastIndexOf(str) ->  last index in this String where given String begins (-1 if not found)
-    length() -> number of characters in this String
-    startsWith(str) ->  true if this String begins with the other's characters
-    substring(i, j) ->  characters in this String from index i (inclusive) to j (exclusive)
-    toLowerCase(), toUpperCase() -> a new String with all lowercase or uppercase letters
-    String.valueOf(number) -> converts the number (int, float, double etc.) to string
-    Integer.valueOf(String) -> converts the string to int. Could also be Double.valueOf(String)
-    String str = 1 + ""; -> Number + "" -> String.
-     */
+/**
+ * Common String methods:
+ * charAt(i) -> the character in this String at a given index
+ * contains(str) -> true if this String contains the other's characters inside it
+ * endsWith(str) -> true if this String ends with the other's characters
+ * equals(str) -> true if this String is the same as str
+ * equalsIgnoreCase(str) -> true if this String is the same as str, ignoring capitalization
+ * indexOf(str) -> first index in this String where given String begins (-1 if not found)
+ * lastIndexOf(str) ->  last index in this String where given String begins (-1 if not found)
+ * length() -> number of characters in this String
+ * startsWith(str) ->  true if this String begins with the other's characters
+ * substring(i, j) ->  characters in this String from index i (inclusive) to j (exclusive)
+ * toLowerCase(), toUpperCase() -> a new String with all lowercase or uppercase letters
+ * String.valueOf(number) -> converts the number (int, float, double etc.) to string
+ * Integer.valueOf(String) -> converts the string to int. Could also be Double.valueOf(String)
+ * String str = 1 + ""; -> Number + "" -> String.
+ */
 
 public class StringCore {
     public static void main(String[] args) {
@@ -50,19 +50,22 @@ public class StringCore {
         String mySubStr = myString.substring(myString.length() - 3, myString.length());
         System.out.println(mySubStr);
 
+        //Check if a character is letter or digit
+        String str = "Person";
+        boolean b = Character.isLetterOrDigit(str.charAt(0));
 
         //Reversing a string
         String s = "Hello world!";
         char[] stringArray = s.toCharArray();
         char[] resultArray = new char[stringArray.length];
-        for (int i = stringArray.length-1; i >= 0; i--){
-            resultArray[stringArray.length-1-i] = stringArray[i];
+        for (int i = stringArray.length - 1; i >= 0; i--) {
+            resultArray[stringArray.length - 1 - i] = stringArray[i];
         }
         System.out.println(new String(resultArray));
 
         //reversing string - approach 2- with constant space
-        for (int i = 0; i < stringArray.length / 2 ; i++){
-            int j = stringArray.length-1-i;
+        for (int i = 0; i < stringArray.length / 2; i++) {
+            int j = stringArray.length - 1 - i;
             char temp = stringArray[i];
             stringArray[i] = stringArray[j];
             stringArray[j] = temp;
--- src/main/java/ds_algo/recursion/Palindrome.java
+++ src/main/java/ds_algo/recursion/Palindrome.java
@@ -3,13 +3,15 @@ package ds_algo.recursion;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class Palindrome {
+    //Steps- declate the vars that we need. Call the helper func with those things. return the result.
     public static boolean isPalindromeWithHelper(String s) {
-        s= s.toUpperCase();
+        s = s.toUpperCase();
         char firstLetter = s.charAt(0);
         char lastLetter = s.charAt(s.length() - 1);
         return isPalindromeHelper(firstLetter, lastLetter, s);
     }
 
+    //private helper func. Especially useful when a var/list etc. need to be maintained
     private static boolean isPalindromeHelper(char firstLetter, char lastLetter, String s) {
         if (s.length() == 0 || s.length() == 1) return true;
         if (firstLetter != lastLetter) return false;
@@ -17,15 +19,30 @@ public class Palindrome {
         return isPalindromeHelper(reducedString.charAt(0), reducedString.charAt(reducedString.length() - 1), reducedString);
     }
 
+    //Direct Recursion
     public static boolean isPalindromeDirect(String s) {
-        s= s.toUpperCase();
+        s = s.toUpperCase();
         if (s.length() <= 1) return true;
         if (s.charAt(0) != s.charAt(s.length() - 1)) return false;
         return isPalindromeDirect(s.substring(1, s.length() - 1));
     }
 
+    //Iterative solution using two pointers
+    public static boolean isPalindromeIterative(String s) {
+        s = s.toUpperCase();
+        int i = 0;
+        int j = s.length() - 1;
+
+        while (i < j) {
+            if (s.charAt(i) != s.charAt(j)) return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
-        assertThat(isPalindromeDirect("moToR")).isEqualTo(isPalindromeWithHelper("moToR")).isEqualTo(false);
-        assertThat(isPalindromeDirect("rotoR")).isEqualTo(isPalindromeWithHelper("RoToR")).isEqualTo(true);
+        assertThat(isPalindromeDirect("moToR")).isEqualTo(isPalindromeWithHelper("moToR")).isEqualTo(isPalindromeIterative("moToR")).isEqualTo(false);
+        assertThat(isPalindromeDirect("rotoR")).isEqualTo(isPalindromeWithHelper("RoToR")).isEqualTo(isPalindromeIterative("roToR")).isEqualTo(true);
     }
 }
