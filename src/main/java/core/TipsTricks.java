e7f3e948c2e0e0895ce9227db00d598afdd78391
Raqeebul Ketan <bijoy.ketan@gmail.com>
2020-05-23 18:34:58 -0400

    Implemented EC_TripletsWithSmallerSum using two pointers

--- src/main/java/core/TipsTricks.java
+++ src/main/java/core/TipsTricks.java
@@ -2,7 +2,7 @@ package core;
 
 import java.util.*;
 
-public class TricksTips {
+public class TipsTricks {
     public static void main(String[] args) {
         //Set ups
         Queue<Integer> queue = new LinkedList<>();
@@ -29,12 +29,19 @@ public class TricksTips {
         int a = 10, b = 20;
 
         //If else differences
-        testIfElse(); //
+        //testIfElse();
         //testIfElse2();
+
+        //Order of execution in a conditional statement (if/while). Left to right
+        //while (start < end && arr[start] == arr[start + 1])
+        //NOT the same as while (arr[start] == arr[start + 1] && start < end )
+        //Esp with indices (start+1) be careful to check when it gets out of bounds
+
+
     }
 
     //If else execution differences
-    public static void testIfElse() {
+    public static void testIfElse() { //executes all the statements that are satisfied by condition
         for (int i = 0; i < 10; i += 2) { //0, 2, 4, 6, 8
             if (i < 5) System.out.printf(" %s < 5, \t", i);
             if (i < 6) System.out.printf(" %s < 6, \t", i);
@@ -43,8 +50,16 @@ public class TricksTips {
             System.out.println(" Final statement");
         }
     }
+    //Result of testIfElse()
+    /*
+     0 < 5, 	 0 < 6, 	 0 < 7, 	 0 < 8, 	 Final statement
+     2 < 5, 	 2 < 6, 	 2 < 7, 	 2 < 8, 	 Final statement
+     4 < 5, 	 4 < 6, 	 4 < 7, 	 4 < 8, 	 Final statement
+     6 < 7, 	 6 < 8, 	 Final statement
+    Final statement
+    */
 
-    public static void testIfElse2() {
+    public static void testIfElse2() {//Executes only the first statement, from top to bottom, that is satisfied
         for (int i = 0; i < 10; i += 2) { //0, 2, 4, 6, 8
             if (i < 5) System.out.printf(" %s < 5, \t", i);
             else if (i < 6) System.out.printf(" %s < 6, \t", i);
@@ -53,4 +68,6 @@ public class TricksTips {
             else System.out.println(" Final statement");
         }
     }
+    // Result of testIfElse2()
+    //  0 < 5, 	 2 < 5, 	 4 < 5, 	 6 < 7, 	 Final statement
 }
---
+++ src/main/java/ds_algo/two_pointer/EC_TripletsWithSmallerSum.java
@@ -0,0 +1,36 @@
+package ds_algo.two_pointer;
+
+import java.util.Arrays;
+
+//https://www.educative.io/courses/grokking-the-coding-interview/mElknO5OKBO
+public class EC_TripletsWithSmallerSum {
+    //arr[i] + arr[j] + arr[k] < target
+    //Find the count of such triplets
+    public static int searchTriplets(int[] arr, int target) {
+        //iterate through the arr
+        //for each ith item
+        //two pointer sum for the elements to the right of ith item
+        //if a triplet is found such that sum triplet < target
+        //then count += end-start (since for a given start, all the items to left od end also satisfy the condition)
+
+        int count = 0;
+        Arrays.sort(arr);
+        for (int i = 0; i < arr.length - 2; i++) {
+            int start = i + 1;
+            int end = arr.length - 1;
+            while (start < end) {
+                if (arr[i] + arr[start] + arr[end] < target) {
+                    count += end - start;
+                    start++;
+                } else end--;
+            }
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(searchTriplets(new int[]{-1, 0, 2, 3}, 3));//2
+        System.out.println(searchTriplets(new int[]{-1, 4, 2, 1, 3}, 5)); //4
+    }
+}
