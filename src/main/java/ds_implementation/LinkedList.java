075288afca223711aa6f853047c419b55d9ee0d4
Raqeebul Ketan <bijoy.ketan@gmail.com>
2020-07-24 15:03:05 -0400

    Refactored the implementation with just head node and added test case.

--- src/main/java/ds_implementation/LinkedList.java
+++ src/main/java/ds_implementation/LinkedList.java
@@ -1,45 +1,50 @@
 package ds_implementation;
 
-//Implement a linked List
-//Expected code that should work
-// LinkedList linkedList = new LinkedList();
+// Implement a linked List. The head should describe the entire list.
+// Expected code that should work
+// LinkedList linkedList = new LinkedList(); => Creation of new LinkedList
 // linkedList.add(5).add(7).add(9);
 // linkedList.print(); => 5,7,8
 // linkedList.reverse();
 // linkedList.print() => 8,7,5
 
+import org.junit.jupiter.api.Test;
+
 public class LinkedList {
 
+    //Node class to contain a value and a pointer to the next node.
     static class Node {
-
         int data;
-        Node next;
+        Node next; //by default next is null
 
         public Node(int data) {
             this.data = data;
         }
     }
 
+    //Head should describe the entire linked list
     private Node head;
-    private Node tail;
 
-    public LinkedList add(int data) {
+    //Adding item to linkedList at the end.
+    public LinkedList addLast(int data) {
         Node newNode = new Node(data);
-        if (head == null && tail == null) {
+        if (head == null) {
             head = newNode;
-            tail = newNode;
-            head.next = tail;
         } else {
-            tail.next = newNode;
-            tail = newNode;
+            Node temp = head;
+            while (temp.next != null) {
+                temp = temp.next;
+            }
+            temp.next = newNode;
         }
         return this;
     }
 
+    //Printing the items of linked list
     public static void printList(LinkedList ls) {
         Node temp = ls.head;
         while (temp != null) {
-            if (temp.next != null){
+            if (temp.next != null) {
                 System.out.print(temp.data + ",");
             } else {
                 System.out.println(temp.data);
@@ -48,9 +53,14 @@ public class LinkedList {
         }
     }
 
-    public static void main(String[] args) {
-        LinkedList ls = new LinkedList(); //Initialization
-        ls.add(5).add(7).add(8);
-        printList(ls); //Printing the values of the linked list ls
+    public static LinkedList reverseList(LinkedList head) {
+        return null;
+    }
+
+    @Test
+    public void testLinkedList() {
+        LinkedList ls = new LinkedList();
+        ls.addLast(4).addLast(5).addLast(6);
+        printList(ls);
     }
 }
