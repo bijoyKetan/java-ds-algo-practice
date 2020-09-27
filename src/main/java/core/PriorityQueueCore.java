5dda95e3f87af806931b0620e58a32c3403477a8
Raqeebul Ketan <bijoy.ketan@gmail.com>
2020-09-26 23:35:33 -0400

    Added the core methods of Priority queue.

---
+++ src/main/java/core/PriorityQueueCore.java
@@ -0,0 +1,22 @@
+package core;
+
+import java.util.PriorityQueue;
+
+public class PriorityQueueCore {
+    public static void main(String[] args) {
+
+        PriorityQueue<String> minHeap = new PriorityQueue<>(5, (s1, s2) -> s1.length() - s2.length());
+        minHeap.offer("Tom");
+        minHeap.offer("Cathy");
+        minHeap.offer("Robert");
+        minHeap.offer("Bo");
+        minHeap.size(); //4
+        //[Bo, Tom, Robert, Cathy]
+
+        minHeap.peek(); //Bo
+        minHeap.poll(); //Bo
+        // Post polling ->  Tom, Cathy, Robert
+    }
+
+
+}
