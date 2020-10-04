229383f8966948ed7271d307bbf190d05162f3cf
Raqeebul Ketan <bijoy.ketan@gmail.com>
2020-10-03 20:00:47 -0400

    Refactored TipsTricks

--- src/main/java/core/TipsTricks.java
+++ src/main/java/core/TipsTricks.java
@@ -1,47 +1,41 @@
 package core;
 
+import org.junit.jupiter.api.Test;
+
 import java.util.*;
 
 public class TipsTricks {
-    public static void main(String[] args) {
+
+    @Test
+    public void iteratorExample() {
         //Set ups
         Queue<Integer> queue = new LinkedList<>();
         queue.offer(3);
         queue.offer(9);
 
+        //Iterate over a collection
+        Iterator<Integer> it = queue.iterator();
+        while (it.hasNext()) {
+            var a = it.next();
+            System.out.println("Queue item: " + a);
+        }
+
         Map<String, String> map = new HashMap<>();
         map.put("Name", "Kyle");
         map.put("Location", "NY");
         map.put("Car", "Toyota");
-
-        //Iterate over a collection
-        Iterator<Integer> it = queue.iterator();
-        while (it.hasNext()) {
-            System.out.println("Queue item: " + it.next());
-        }
-
-        //Iterate over a map
+        //Iterate over a map with iterator
         for (Map.Entry<String, String> e : map.entrySet()) {
-            System.out.format("%s : %s\n", e.getKey(), e.getValue());
+            var key = e.getKey();
+            var value = e.getValue();
+            System.out.format("%s : %s\n", key, value);
         }
-
-        //Declare multiple variables in the same line
-        int a = 10, b = 20;
-
-        //If else differences
-        //testIfElse();
-        //testIfElse2();
-
-        //Order of execution in a conditional statement (if/while). Left to right
-        //while (start < end && arr[start] == arr[start + 1])
-        //NOT the same as while (arr[start] == arr[start + 1] && start < end )
-        //Esp with indices (start+1) be careful to check when it gets out of bounds
-
-
     }
 
-    //If else execution differences
-    public static void testIfElse() { //executes all the statements that are satisfied by condition
+
+    @Test
+    public void ifElseExample() {
+        //CASE 1 - Multiple if: each condition is checked independently
         for (int i = 0; i < 10; i += 2) { //0, 2, 4, 6, 8
             if (i < 5) System.out.printf(" %s < 5, \t", i);
             if (i < 6) System.out.printf(" %s < 6, \t", i);
@@ -49,17 +43,19 @@ public class TipsTricks {
             if (i < 8) System.out.printf(" %s < 8, \t", i);
             System.out.println(" Final statement");
         }
-    }
-    //Result of testIfElse()
-    /*
-     0 < 5, 	 0 < 6, 	 0 < 7, 	 0 < 8, 	 Final statement
-     2 < 5, 	 2 < 6, 	 2 < 7, 	 2 < 8, 	 Final statement
-     4 < 5, 	 4 < 6, 	 4 < 7, 	 4 < 8, 	 Final statement
-     6 < 7, 	 6 < 8, 	 Final statement
-    Final statement
-    */
+        //CASE 1 RESULT
+        /*
+         0 < 5, 	 0 < 6, 	 0 < 7, 	 0 < 8, 	 Final statement
+         2 < 5, 	 2 < 6, 	 2 < 7, 	 2 < 8, 	 Final statement
+         4 < 5, 	 4 < 6, 	 4 < 7, 	 4 < 8, 	 Final statement
+         6 < 7, 	 6 < 8, 	 Final statement
+        Final statement
+        */
 
-    public static void testIfElse2() {//Executes only the first statement, from top to bottom, that is satisfied
+        System.out.println("\n\n");
+
+        //CASE 2 - if + else if: each else if is dependent on the previous if/ else if condition
+        //and will be executed only if the previous checks are not satisfied
         for (int i = 0; i < 10; i += 2) { //0, 2, 4, 6, 8
             if (i < 5) System.out.printf(" %s < 5, \t", i);
             else if (i < 6) System.out.printf(" %s < 6, \t", i);
@@ -67,7 +63,38 @@ public class TipsTricks {
             else if (i < 8) System.out.printf(" %s < 8, \t", i);
             else System.out.println(" Final statement");
         }
+        //CASE 2 RESULT
+        //  0 < 5, 	 2 < 5, 	 4 < 5, 	 6 < 7, 	 Final statement
     }
-    // Result of testIfElse2()
-    //  0 < 5, 	 2 < 5, 	 4 < 5, 	 6 < 7, 	 Final statement
+
+    @Test
+    public void miscTricks() {
+        int a = 10, b = 20;
+        System.out.println(String.format("A is %s, and B is %s", a, b));
+    }
+
+    @Test
+    public void orderOfExecution() {
+        int[] arr = new int[]{1, 2, 3, 4};
+
+        int i = 0;
+        while (i < arr.length) {
+            //if (i < arr.length - 1 && i % 2 == 0) {
+            if (i % 2 == 0 && i < arr.length - 1) {
+                var num = arr[i + 1];
+                System.out.println(num);
+                i++;
+            } else {
+                i++;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        //Order of execution in a conditional statement (if/while). Left to right
+        //while (start < end && arr[start] == arr[start + 1])
+        //NOT the same as while (arr[start] == arr[start + 1] && start < end )
+        //Esp with indices (start+1) be careful to check when it gets out of bounds
+    }
 }
