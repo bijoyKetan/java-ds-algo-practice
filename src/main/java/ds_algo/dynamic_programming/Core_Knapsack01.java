d1af21f77a3ca827498e0abe8d8f4efdf641e80d
Raqeebul Ketan <bijoy.ketan@gmail.com>
2020-11-27 18:18:16 -0500

    Solved 0/1 Knapsack problem using bottom up dynamic programming.

--- src/main/java/ds_algo/dynamic_programming/Core_Knapsack01.java
+++ src/main/java/ds_algo/dynamic_programming/Core_Knapsack01.java
@@ -45,14 +45,64 @@ public class Core_Knapsack01 {
         return maxProfit;
     }
 
+    //Decision making DP template
+    public int solveKnapsackDp(int[] profits, int[] weights, int capacity) {
+        //1. Build target from 0 to t.
+        //Here target is capacity
+        int m = profits.length;
+
+        //2. dp [array length of items, i.e. bag size of values][target +1]
+        //Note: Row - Items. Row -> indexing used in recursion
+        //Col -> target (0 to t)
+        int[][] dp = new int[m][capacity + 1];
+
+        //3.Initialize array.
+        //Note: No need to initialize row/col where the entire row/col == 0
+        // int[][] automatically initializes entire array with 0
+
+        //Initialize the set of values/items that will be picked/not picked
+        //Initialize 0th row by iterating through range(0, target+1)
+        for (int i = 0; i <= capacity; i++) {
+            if (weights[0] <= i) {
+                dp[0][i] = profits[0];
+            }
+        }
+
+        //4. Process the sub-arrays of all targets
+        for (int row = 1; row < m; row++) { //index of items
+            for (int col = 1; col <= capacity; col++) { //building target value
+
+                int profit0 = 0, profit1 = 0;
+                //Option 1: Do not pick
+                profit0 = dp[row - 1][col];
+
+                //Option 2: Pick, only if weight constraint not violated
+                //Profit = profit from this item + Max profit from previous items
+                if (weights[row] <= col) {
+                    profit1 = profits[row] + dp[row - 1][col - weights[row]];
+                }
+
+                //Get the max of p0 and p1
+                dp[row][col] = Math.max(profit0, profit1);
+            }
+        }
+        //Return the last item
+        return dp[m - 1][capacity];
+    }
+
 
     @Test
     public void test() {
         int[] profits = {1, 6, 10, 16};
         int[] weights = {1, 2, 3, 5};
         int maxProfit = solveKnapsackRecursive(profits, weights, 7);
-        System.out.println("Total knapsack profit ---> " + maxProfit);//22
+        System.out.println("Total knapsack profit 1 with recursion ---> " + maxProfit);//22
         maxProfit = solveKnapsackRecursive(profits, weights, 6);
-        System.out.println("Total knapsack profit ---> " + maxProfit);//17
+        System.out.println("Total knapsack profit 2 with recursion ---> " + maxProfit);//17
+
+        int maxProfit2 = solveKnapsackDp(profits, weights, 7);
+        System.out.println("Total knapsack profit 1 with DP ---> " + maxProfit2);//22
+        maxProfit2 = solveKnapsackDp(profits, weights, 6);
+        System.out.println("Total knapsack profit 2 with DP ---> " + maxProfit2);//17
     }
 }
