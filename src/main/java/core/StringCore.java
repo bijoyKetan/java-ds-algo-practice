6fe612c7efe6d85d0cc57b93bc2cf0235e74da8e
Raqeebul Ketan <bijoy.ketan@gmail.com>
2020-12-24 13:50:26 -0500

    Modified the core string methods.

--- src/main/java/core/StringCore.java
+++ src/main/java/core/StringCore.java
@@ -7,58 +7,79 @@ public class StringCore {
 
     @Test
     public void coreStringMethods() {
-        String str = "A word";
+        String str = "  A word  ";
 
+        str.trim(); //leading and trailing spaces removed -> "A word"
         str.charAt(0); // char in this String at i
         str.contains("targetStr");// -> boolean
         str.startsWith("targetStr");//->  boolean
         str.endsWith("targetStr"); // -> boolean
         str.equals("targetStr");// -> case sensitive equality
         str.equalsIgnoreCase("targetStr");//-> case insensitive equality check
-        str.indexOf("targetStr");//-> first index where str starts (else -1)
-        str.lastIndexOf("targetStr");//->  last index where str starts (else -1)
         str.length();//-> number of chars
         str.substring(0, str.length());//->  i (inclusive) to j (exclusive)
         str.toLowerCase();//-> new str with all lowercase
         str.toUpperCase();//-> new str with all uppercase
-        String.valueOf(123);//-> number to string
-        String.valueOf('c');//-> character to string
-        Integer.valueOf("12345");//-> String to integer
-        Double.valueOf("123.45");//-> String to double
         String str2 = 1 + "";//-> Number + "" -> String.
         str.toCharArray(); //-> convert string to charArray
+        str.indexOf("targetStr");//-> first index where str starts (else -1)
+        str.lastIndexOf("targetStr");//->  last index where str starts (else -1)
 
-        //Strings are immutable in Java
-        //For string manipulation, use StringBuilder instead
-        //While dealing with string input, we must need additional space
-        //due to string immutability
-        StringBuilder sb = new StringBuilder();
-        sb.append("ab");
-        sb.append("c");
-        sb.toString(); //-> returns string, abc
-    }
 
-    public static void main(String[] args) {
-
+        // *** REPLACEMENT *** //
+        String sentence = "It's a man, it's a plane, it's superman.";
+        // Task: convert to lowercase and remove punctuations
+        sentence = sentence
+                .toLowerCase()
+                .replaceAll("[,_!'.]", "");
+        /*
+        its a man its a plane its superman
+        No need of escape characters inside []
+        Each character is treated as literal inside [] and replaced
+        Without [], escape character \\ needed e.g.
+        sentence = sentence.replaceAll("\\.", "");
+         */
         String a = " New York  "; //Initialized -> " New York  "
-        a = a.trim(); //leading and trailing spaces removed -> "New York"
+        a = a.trim(); //"New York"
         a = a.replace(' ', '*'); // character replaced -> New*York
         a = a.replace("ew", "!!"); // seq of chars replaced -> N!!*York
         a = a.toUpperCase(); // -> N!!*YORK
         a = a.toLowerCase(); // -> n!!*york
 
-        //Replacement
-        String sentence = "It's a man, it's a plane, it's superman.";
-        //convert to lowercase and remove punctuations
-        sentence = sentence
-                .toLowerCase()
-                .replaceAll("[,_!'.]", "");
-        //its a man its a plane its superman
-        //No need of escape characters inside []
-        //Each character is treated as literal inside [] and replaced
-        //Without [], escape character \\ needed e.g.
-        //sentence = sentence.replaceAll("\\.", "");
 
+        // *** TYPE CONVERSIONS *** //
+        String.valueOf(123);//-> number to string
+        String.valueOf('c');//-> character to string
+
+        Integer.parseInt("12345"); //String to int, preferred
+        //Integer.valueOf("12345");
+
+        Double.parseDouble("123.45"); //-> String to double, preferred
+        //Double.valueOf("123.45");
+
+
+        // *** STRING BUILDER *** //
+        /* Strings are immutable in Java
+        For string manipulation, use StringBuilder instead */
+        StringBuilder sb = new StringBuilder("California");
+        //StringBuilder sb = new StringBuilder();
+        //sb.append("California");
+        sb.append(", CA"); // -> California, CA
+        sb.reverse(); // -> AC ,ainrofilaC
+        sb.reverse(); // -> California, CA
+        sb.deleteCharAt(sb.length() - 1); // -> California, C
+        sb.delete(sb.indexOf(","), sb.lastIndexOf("C") + 1);
+        // same as -> b.delete(10, 12+1); -> California
+        sb.toString(); // California
+
+
+        // *** MISC *** //
+        //Is a given character letter or digit
+        String str3 = "This is 2019!";
+        Character.isLetterOrDigit(str.charAt(0)); // true
+    }
+
+    public static void main(String[] args) {
 
         /*
         Note: String in Java is immutable, but StringBuilder is immutable
@@ -105,30 +126,5 @@ public class StringCore {
         bool = z.equalsIgnoreCase("SAMPLE String..."); // -> true
         String zSubString = z.substring(1, z.length() - 1); // inclusive start and exclusive start -> ample String..
 
-        //***************//
-        //VARIOUS TRICKS//
-        //***************//
-        //Is a given character letter or digit
-        String str = "This is 2019!";
-        bool = Character.isLetterOrDigit(str.charAt(0)); // true
-
-        int num = 5;
-        String strNum = "" + num; //converts to string
-
-        String someNum = "33";
-        int someNumInt = Integer.parseInt(someNum); // -> 33
-        System.out.println(someNum);
-
-
-        //*****************//
-        //  STRING BUILDER //
-        //*****************//
-        //For multiple manipulations, use StringBuilder
-        StringBuilder b = new StringBuilder("California");
-        b.append(", CA"); // -> California, CA
-        b.reverse(); // -> AC ,ainrofilaC
-        b.reverse(); // -> California, CA
-        b.deleteCharAt(b.length() - 1); // -> California, C
-        b.delete(b.indexOf(","), b.lastIndexOf("C") + 1); // same as -> b.delete(10, 12+1); -> California
     }
 }
