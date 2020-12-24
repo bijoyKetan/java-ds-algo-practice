49493eabf0de8279954e7389324866b5a4bbd693
Raqeebul Ketan <bijoy.ketan@gmail.com>
2020-12-24 17:17:03 -0500

    Solved LC451_SortCharactersByFrequency using bucket sort technique.

--- src/main/java/ds_algo/string/LC451_SortCharactersByFrequency.java
+++ src/main/java/ds_algo/string/LC451_SortCharactersByFrequency.java
@@ -38,9 +38,55 @@ public class LC451_SortCharactersByFrequency {
         return sb.toString();
     }
 
+    public String frequencyBucketSort(String s) {
+        /*
+        Create freq map of characters
+        Keep track of max freq
+        Create an array of size maxFreq
+        Iterate through map and put the characters in array
+        Iterate through array in reverse order and build the word
+         */
+
+        if (s == null || s.length() < 2) return s;
+        Map<Character, Integer> map = new HashMap<>();
+        int maxFreq = 0;
+
+        for (Character c : s.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+            //maxFreq = Math.max(maxFreq, map.get(c));
+            if (maxFreq < map.get(c)) maxFreq = map.get(c);
+        }
+
+        StringBuilder[] arr = new StringBuilder[maxFreq + 1];
+        //Note: For objects java created arr with nulls
+        //Note: For map iteration, keySet is enough, no need for entrySet()
+        for (Character c : map.keySet()) {
+            int freq = map.get(c);
+            StringBuilder sb = new StringBuilder();
+            while (freq > 0) {
+                sb.append(c);
+                freq--;
+            }
+            //Null checks very important for string builder
+            arr[map.get(c)] = arr[map.get(c)] != null ? sb.append(arr[map.get(c)]) : sb;
+        }
+
+        StringBuilder result = new StringBuilder();
+        for (int i = arr.length - 1; i > 0; i--) {
+            if (arr[i] != null) {
+                result.append(arr[i]);
+            }
+        }
+        return result.toString();
+    }
+
+
     @Test
     void testFrequencySort() {
         assertThat(frequencySort("tree")).isEqualTo("eert");
         assertThat(frequencySort("Aabb")).isEqualTo("bbAa");
+
+        assertThat(frequencyBucketSort("tree")).isIn("eert", "eetr");
+        assertThat(frequencyBucketSort("Aabb")).isIn("bbAa", "bbaA");
     }
 }
