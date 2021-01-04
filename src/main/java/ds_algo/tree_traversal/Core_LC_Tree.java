370bdf4c51030e9e343072772a84e1b76546a93b
Raqeebul Ketan <bijoy.ketan@gmail.com>
2021-01-03 23:21:28 -0500

    Solved https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/

--- src/main/java/ds_algo/tree_traversal/Core_LC_Tree.java
+++ src/main/java/ds_algo/tree_traversal/Core_LC_Tree.java
@@ -517,4 +517,24 @@ public class Core_LC_Tree {
         invertTree(root.right);
         return root;
     }
+
+
+    //******************//
+    //108. Convert Sorted Array to Binary Search Tree
+    //https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/
+    //******************//
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return helperSortedArrayToBST(0, nums.length-1, nums);
+
+    }
+
+    private TreeNode helperSortedArrayToBST(int left, int right, int[] nums){
+        if (left > right) return null;
+        int mid = left + (right - left) /2;
+
+        TreeNode root = new TreeNode(nums[mid]);
+        root.left = helperSortedArrayToBST(left, mid - 1, nums);
+        root.right = helperSortedArrayToBST(mid + 1, right, nums);
+        return root;
+    }
 }
