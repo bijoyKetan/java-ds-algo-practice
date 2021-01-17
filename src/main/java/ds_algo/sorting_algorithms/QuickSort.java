3750e78745cc9f4fca49c3812f3425b4bb37339d
Raqeebul Ketan <bijoy.ketan@gmail.com>
2021-01-17 00:58:59 -0500

    Refactored quick sort implementation.

--- src/main/java/ds_algo/sorting_algorithms/QuickSort.java
+++ src/main/java/ds_algo/sorting_algorithms/QuickSort.java
@@ -3,11 +3,6 @@ package ds_algo.sorting_algorithms;
 import java.util.Arrays;
 
 public class QuickSort {
-    public static void main(String[] args) {
-        int[] input = new int[]{4, 7, 2, 9, 9, 8, 10};
-        int[] sorted = quickSort(input);
-        System.out.println(Arrays.toString(sorted));
-    }
 
     public static int[] quickSort(int[] arr) {
         //Edge cases
@@ -24,26 +19,35 @@ public class QuickSort {
         int pivot = right;
         int wall = left;
 
-        // Iterate from left through pivot -1
+        // Iterate from left through pivot-1
         // Everything to left of wall is smaller
         // Everything to right of wall is bigger or equal
         for (int i = left; i < right; i++) {
             if (arr[i] < arr[pivot]) {
-                //Swap (wall, i)
-                int temp = arr[i];
-                arr[i] = arr[wall];
-                arr[wall] = temp;
-                wall++;
+                swap(arr, wall++, i);
             }
         }
-
         //Finally swap (pivot, wall)
-        int temp = arr[pivot];
-        arr[pivot] = arr[wall];
-        arr[wall] = temp;
+        swap(arr, pivot, wall);
 
         //Recursive case
         helperQuickSort(arr, left, wall - 1);
         helperQuickSort(arr, wall + 1, right);
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public static void main(String[] args) {
+        int[] input = new int[]{4, 7, 2, 9, 9, 8, 10};
+        int[] input2 = new int[]{9, 1, 6, 10, 0, 18, 9, 8, 5, 6, 3, 22, -10, -8, 2};
+
+        int[] sorted = quickSort(input);
+        int[] sorted2 = quickSort(input2);
+        System.out.println(Arrays.toString(sorted));
+        System.out.println(Arrays.toString(sorted2));
     }
 }
