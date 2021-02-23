899ac7565219ca97be86ee1aba1b05ba8ea4de88
Raqeebul Ketan <bijoy.ketan@gmail.com>
2021-02-22 21:31:03 -0500

    Solved 40. Combination Sum II

--- src/main/java/ds_algo/backtracking/BackTrackingQuestions.java
+++ src/main/java/ds_algo/backtracking/BackTrackingQuestions.java
@@ -87,4 +87,45 @@ public class BackTrackingQuestions {
     }
 
     /////////////////////////////////////////////////////////////////////
+
+    /*
+    40. Combination Sum II
+    Given a collection of candidate numbers (candidates) and a target number (target),
+    find all unique combinations in candidates where the candidate numbers sum to target.
+    Each number in candidates may only be used once in the combination
+    Input: candidates = [10,1,2,7,6,1,5], target = 8
+    Output:
+    Input: candidates = [2,5,2,1,2], target = 5
+    Output:
+    [[1,2,2], [5]]
+    */
+    public static List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        Arrays.sort(candidates);
+        List<List<Integer>> result = new ArrayList<>();
+        Deque<Integer> innerDeque = new ArrayDeque<>();
+        helper(candidates, target, result, innerDeque, 0);
+        return result;
+    }
+
+    private static void helper(int[] candidates, int target, List<List<Integer>> result, Deque<Integer> innerDeque, int index) {
+
+        if (target < 0) return;
+        else if (target == 0) {
+            result.add(new ArrayList<>(innerDeque));
+            return;
+        }
+
+        for (int i = index; i < candidates.length; i++) {
+            if (candidates[i] > target) break;
+            if (i > index && candidates[i] == candidates[i - 1]) continue;
+            innerDeque.add(candidates[i]);
+            helper(candidates, target - candidates[i], result, innerDeque, i + 1);
+            innerDeque.removeLast();
+        }
+    }
+
+    @Test
+    void combinationSum2Test() {
+        System.out.println(combinationSum2(new int[]{2, 5, 2, 1, 2}, 5)); //[[1, 2, 2], [5]]
+    }
 }
